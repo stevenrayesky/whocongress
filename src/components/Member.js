b5702af26c7832049c1a3628703c59083c545f47
Steven Rayesky <protolexus@gmail.com>
2018-06-15 15:14:48 -0400

    extended component class to Member to pass down props.

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import '../css/member.css';
 
-const Member = ({member}) =>
-  <div className="tile" key={member.id}>
-    <h4>{member.short_title} {member.first_name} {member.last_name}</h4>
-    <p>Office: {member.office}</p>
-  </div>
+class Member extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const key = this.props.member.id;
+    return(
+      <div className="tile" onClick={(e) => this.props.onClick(e, key)} key={key}>
+        <h4 className="member-name">
+          {this.props.member.short_title} {this.props.member.first_name} {this.props.member.last_name}
+        </h4>
+        <div className="tile__info">
+        <p>Office: {this.props.member.office}</p>
+        </div>
+      </div>
+    )
+  }
+  
+}
 
 export default Member
